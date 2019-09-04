2ab8b9aecc910dfe59ac45c67e68ce67026838d6
Jake Lekas <jlekas33@gmail.com>
2019-09-04 19:37:15 -0400

    starting project with gatsby js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Recipe Site`,
+    description: `This is the site with the best recipes on the internet.`,
+    author: `@jlekas`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
