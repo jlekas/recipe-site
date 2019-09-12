1cd7f2c3b245d3c5f8cfa250fa02cb5e0b0a0845
Jake Lekas <jlekas33@gmail.com>
2019-09-11 20:20:23 -0400

    no pyc in hur

---
+++ src/pages/results.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+//import { Link } from "gatsby";
+
+
+const ResultsPage = () => (
+  <Container>
+    <Row>
+      <Col>
+        <h1 style={{"font-size": "4em"}}>Boom Recipe</h1>
+      </Col>
+    </Row>
+    <Row>
+      <Col>
+        <h1 style={{"font-size": "4em"}}>Results Page</h1>
+      </Col>
+    </Row>
+  </Container>
+);
+
+export default ResultsPage;
