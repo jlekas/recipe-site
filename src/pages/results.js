95fa44a7746d1acfe6dd09a126cb1843d0db5dab
Jake Lekas <jlekas33@gmail.com>
2019-09-23 19:57:32 -0400

    added back button

--- src/pages/results.js
+++ src/pages/results.js
@@ -4,7 +4,8 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import styles from "../styles/results.module.css";
 import RecipeBox from "../components/RecipeBox";
-//import { Link } from "gatsby";
+import { Link } from "gatsby";
+import Button from "react-bootstrap/Button";
 
 const ResultsPage = () => (
   <Container className={styles.container}>
@@ -17,6 +18,11 @@ const ResultsPage = () => (
         <RecipeBox></RecipeBox>
         <RecipeBox></RecipeBox>
         <RecipeBox></RecipeBox>
+    </Row>
+    <Row>
+      <Link to="/">
+        <Button variant="outline-secondary" className={styles.btn}>Back to Search</Button>
+      </Link>
     </Row>
     <Row>
     </Row>
