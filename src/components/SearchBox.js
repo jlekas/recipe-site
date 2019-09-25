eb095e24fa2d2192ac95e1ddfdfac1931f254a5e
Jake Lekas <jlekas33@gmail.com>
2019-09-25 19:26:55 -0400

    search button and form finally same size :)

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -2,14 +2,14 @@ import React from "react";
 import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
 import Button from "react-bootstrap/Button";
-import styles from "../styles/searchbox.module.css"
-import { Link } from "gatsby"
+import styles from "../styles/searchbox.module.css";
+import { Link } from "gatsby";
 
 console.log("saerch", styles)
 
 const SearchBox = () => (
   <div className={styles.searchBox}>
-    <InputGroup size="lg">
+    <InputGroup size="lg-3">
       <FormControl
         placeholder="Search By Ingredients or Cuisine"
         aria-label="Search By Ingredients or Cuisine"
